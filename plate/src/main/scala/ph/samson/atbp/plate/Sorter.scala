fb333cba8e10fb5822f99df8e1ff1c020a3ea9e1
Edward Samson <edward@samson.ph>
2025-05-01 09:54:58 +0800

    Reformat docs

--- plate/src/main/scala/ph/samson/atbp/plate/Sorter.scala
+++ plate/src/main/scala/ph/samson/atbp/plate/Sorter.scala
@@ -142,153 +142,166 @@ object Sorter {
     }
 
     /** Rerank Jira items in the current list according to the order given by
-     * the target list.
-     * 
-     * Both lists must contain exactly the same elements, in possibly different
-     * orders.
-     *
-     * @param current keys ordered in the current Jira ranking
-     * @param target keys ordered in the desired ranking
-     * @return number of rerankings executed
-     */
+      * the target list.
+      *
+      * Both lists must contain exactly the same elements, in possibly different
+      * orders.
+      *
+      * @param current
+      *   keys ordered in the current Jira ranking
+      * @param target
+      *   keys ordered in the desired ranking
+      * @return
+      *   number of rerankings executed
+      */
     def reorder(current: List[String], target: List[String]): Task[Int] = {
 
       /** Actual work happens here.
-       *
-       * We process the current key order and target key order in a series of
-       * recursive steps and determine the list of rerankings to be executed
-       * on the current key order to arrive at the target key order.
-       *
-       * Let's work through an example.
-       *
-       * Step 0. Initial state.
-       * c = [A, B, C, D, E]
-       * t = [A, D, C, B, E]
-       * a = []
-       * r = []
-       *
-       * Step 1. Order is correct. Nothing to do to A. Proceed with next item.
-       * c = [B, C, D, E]
-       * t = [D, C, B, E]
-       * a = []
-       * r = []
-       *
-       * Step 2. Put current head, B, aside as we look for the match to target
-       * head, D.
-       * c = [C, D, E]
-       * t = [D, C, B, E]
-       * a = [B]
-       * r = []
-       *
-       * Step 3. Still no match, put C aside.
-       * c = [D, E]
-       * t = [D, C, B, E]
-       * a = [C, B]
-       * r = []
-       *
-       * Step 4. Match found. D goes before everything we've put aside. Because
-       * B is the top of the current order, we rerank D > B. Restore the items
-       * we've put aside back to the current order to be processed
-       * c = [B, C, E]
-       * t = [C, B, E]
-       * a = []
-       * r = [D > B]
-       *
-       * Step 5. Put B aside again.
-       * c = [C, E]
-       * t = [C, B, E]
-       * a = [B]
-       * r = [D > B]
-       *
-       * Step 6. Match found. C goes before B.
-       * c = [B, E]
-       * t = [B, E]
-       * a = []
-       * r = [C > B, D > B]
-       *
-       * Steps 7 and 8 find the remaining items to be in the correct order.
-       * The result is the reverse of the accumulated reranks.
-       * return [D > B, C > B]
-       *
-       * @param curRemaining where we are in the current list
-       *                     for example, we start with
-       * @param tarRemaining where we are in the target list
-       *                     for example, we start with
-       * @param aside items in the current list we have set aside to be reranked
-       * @param reranks rerankings to be done
-       * @return rerankings to be executed
-       */
-      def doRerank(curRemaining: List[String], tarRemaining: List[String], aside: List[String], reranks: List[Rerank]): Task[List[Rerank]] = {
+        *
+        * We process the current key order and target key order in a series of
+        * recursive steps and determine the list of rerankings to be executed on
+        * the current key order to arrive at the target key order.
+        *
+        * Let's work through an example.
+        *
+        * Step 0. Initial state.
+        *   - c = [A, B, C, D, E]
+        *   - t = [A, D, C, B, E]
+        *   - a = []
+        *   - r = []
+        *
+        * Step 1. Order is correct. Nothing to do to A. Proceed with next item.
+        *   - c = [B, C, D, E]
+        *   - t = [D, C, B, E]
+        *   - a = []
+        *   - r = []
+        *
+        * Step 2. Put current head, B, aside as we look for the match to target
+        * head, D.
+        *   - c = [C, D, E]
+        *   - t = [D, C, B, E]
+        *   - a = [B]
+        *   - r = []
+        *
+        * Step 3. Still no match, put C aside.
+        *   - c = [D, E]
+        *   - t = [D, C, B, E]
+        *   - a = [C, B]
+        *   - r = []
+        *
+        * Step 4. Match found. D goes before everything we've put aside. Because
+        * B is the top of the current order, we rerank D > B. Restore the items
+        * we've put aside back to the current order to be processed
+        *   - c = [B, C, E]
+        *   - t = [C, B, E]
+        *   - a = []
+        *   - r = [D > B]
+        *
+        * Step 5. Put B aside again.
+        *   - c = [C, E]
+        *   - t = [C, B, E]
+        *   - a = [B]
+        *   - r = [D > B]
+        *
+        * Step 6. Match found. C goes before B.
+        *   - c = [B, E]
+        *   - t = [B, E]
+        *   - a = []
+        *   - r = [C > B, D > B]
+        *
+        * Steps 7 and 8 find the remaining items to be in the correct order.
+        *
+        * Step 9. The result is the reverse of the accumulated reranks.
+        *   - return [D > B, C > B]
+        *
+        * @param curRemaining
+        *   where we are in the current list for example, we start with
+        * @param tarRemaining
+        *   where we are in the target list for example, we start with
+        * @param aside
+        *   items in the current list we have set aside to be reranked
+        * @param reranks
+        *   rerankings to be done
+        * @return
+        *   rerankings to be executed
+        */
+      def doRerank(
+          curRemaining: List[String],
+          tarRemaining: List[String],
+          aside: List[String],
+          reranks: List[Rerank]
+      ): Task[List[Rerank]] = {
         curRemaining match {
-          case Nil => tarRemaining match {
-            case Nil =>
-              // Let's get the simple case out of the way.
-              // We have finished both lists.
-              ZIO.succeed(reranks.reverse)
-            case tarHead :: tarNext =>
-              // I don't know if I expect this to happen.
-              ZIO.fail(new IllegalStateException(s"current list finished while target at $tarHead :: $tarNext"))
-          }
-            
-          case curHead :: curNext => tarRemaining match {
-            case Nil =>
-              // I don't know if I expect this to happen.
-              ZIO.fail(new IllegalStateException(s"target list finished while current at $curHead :: $curNext"))
-            case tarHead :: tarNext =>
-              if (curHead == tarHead) {
-                if (aside.isEmpty) {
-                  // Order is correct, proceed with next item.
-                  //
-                  // For example, we get this on recursion 1:
-                  // c = [B, C, D, E]
-                  // t = [D, B, C, E]
-                  doRerank(curNext, tarNext, Nil, reranks)
+          case Nil =>
+            tarRemaining match {
+              case Nil =>
+                // We have finished both lists.
+                //
+                // This is Step 9 in the example.
+                ZIO.succeed(reranks.reverse)
+              case tarHead :: tarNext =>
+                // I don't expect this to happen.
+                ZIO.fail(
+                  new IllegalStateException(
+                    s"current list finished while target at $tarHead :: $tarNext"
+                  )
+                )
+            }
+
+          case curHead :: curNext =>
+            tarRemaining match {
+              case Nil =>
+                // I don't expect this to happen.
+                ZIO.fail(
+                  new IllegalStateException(
+                    s"target list finished while current at $curHead :: $curNext"
+                  )
+                )
+              case tarHead :: tarNext =>
+                if (curHead == tarHead) {
+                  if (aside.isEmpty) {
+                    // Order is correct, proceed with next item.
+                    //
+                    // This happens in steps 1, 7, and 8 in the example.
+                    doRerank(curNext, tarNext, Nil, reranks)
+                  } else {
+                    // curHead should go next in the order before everything
+                    // that we've put aside so far.
+                    // The rerank is only against the first thing we've set
+                    // aside (restore.head) because we don't know if the other
+                    // items need to be reranked among themselves.
+                    //
+                    // This is happens in steps 4 and 6 in the example.
+                    val restore = aside.reverse
+                    val rerank = Rerank(curHead, restore.head)
+                    doRerank(
+                      restore ++ curNext,
+                      tarNext,
+                      Nil,
+                      rerank :: reranks
+                    )
+                  }
                 } else {
-                  // curHead should go next in the order.
-                  // Everything in aside should be ranked after it.
+                  // Put current head aside and find next match to target.
                   //
-                  // In recursion 4 we restore the keys we set aside.
-                  // Reversed because we accumulated them that way.
-                  //
-                  // We now know that current head should be ranked before the
-                  // restored keys.
-                  //
-                  // c = [B, C, E]
-                  // t = [B, C, E]
-                  // a = []
-                  // r = [D > B]
-                  val restore = aside.reverse
-                  val rerank = Rerank(curHead, restore.head)
-                  doRerank(restore ++ curNext, tarNext, Nil, rerank :: reranks)
+                  // This happens in steps 2, 3, and 5 in the example.
+                  doRerank(curNext, tarRemaining, curHead :: aside, reranks)
                 }
-              } else {
-                // Put current head aside and find next match to target.
-                //
-                // We get this in recursion 2:
-                // c = [C, D, E]
-                // t = [D, B, C, E]
-                // a = [B]
-                //
-                // Then recursion 3:
-                // c = [D, E]
-                // t = [D, B, C, E]
-                // a = [C, B]
-                doRerank(curNext, tarRemaining, curHead :: aside, reranks)
-              }
-          }
+            }
         }
       }
 
       def executeReranks(reranks: List[Rerank]): Task[Int] = {
         // Rerankings before the same key can be done in one call
         val groups = reranks.groupBy(_.lowKey).view.mapValues(_.map(_.highKey))
-        val execs = ZIO.foreachParDiscard(groups) {
-          case (lowKey, highKeys) =>
-            ZIO.logInfo(s"execute rerank $highKeys before $lowKey") *> (if (highKeys.length <= 50) {
-                client.rankIssuesBefore(highKeys, lowKey, None)
-              } else {
-                rerank((highKeys :+ lowKey).grouped(51).toList)
-              })
+        val execs = ZIO.foreachParDiscard(groups) { case (lowKey, highKeys) =>
+          ZIO.logInfo(
+            s"execute rerank $highKeys before $lowKey"
+          ) *> (if (highKeys.length <= 50) {
+                  client.rankIssuesBefore(highKeys, lowKey, None)
+                } else {
+                  rerank((highKeys :+ lowKey).grouped(51).toList)
+                })
         }
 
         execs.as(groups.size)
@@ -296,11 +309,22 @@ object Sorter {
 
       for {
         _ <- ZIO.attempt {
-          require(current.length == target.length, s"current (${current.length}) length not equal to target (${target.length})")
-          require(current.forall(target.contains), s"current not in target: ${current.filterNot(target.contains)}")
-          require(target.forall(current.contains), s"target not in current: ${target.filterNot(current.contains)}")
+          require(
+            current.length == target.length,
+            s"current (${current.length}) length not equal to target (${target.length})"
+          )
+          require(
+            current.forall(target.contains),
+            s"current not in target: ${current.filterNot(target.contains)}"
+          )
+          require(
+            target.forall(current.contains),
+            s"target not in current: ${target.filterNot(current.contains)}"
+          )
         }
-        _ <- ZIO.logInfo(s"reordering\n${current.zip(target).zipWithIndex.mkString("\n")}")
+        _ <- ZIO.logInfo(
+          s"reordering\n${current.zip(target).zipWithIndex.mkString("\n")}"
+        )
         reranks <- doRerank(current, target, Nil, Nil)
         count <- executeReranks(reranks)
       } yield count
@@ -358,8 +382,7 @@ object Sorter {
     } yield LiveImpl(client): Sorter
   }
 
-  /** Command to rank highKey before lowKey.
-   * That is, highKey > lowKey.
-   */
+  /** Command to rank highKey before lowKey. That is, highKey > lowKey.
+    */
   private case class Rerank(highKey: String, lowKey: String)
 }

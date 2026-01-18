883dbc88b2caa71ac83025025bf194b11f442ef6
Edward Samson <edward@samson.ph>
2026-01-18 21:33:16 +0800

    parse

---
+++ stmt2csv/src/main/scala/ph/samson/atbp/stmt2csv/parsers/BpiAccountParser.scala
@@ -0,0 +1,82 @@
+package ph.samson.atbp.stmt2csv.parsers
+
+import fastparse.*
+import ph.samson.atbp.stmt2csv.CsvEntry
+import zio.Task
+import zio.ZIO
+
+import java.time.MonthDay
+import java.time.format.DateTimeFormatter
+
+import NoWhitespace.*
+
+object BpiAccountParser {
+
+  def apply(text: String): Task[List[CsvEntry]] = ZIO.attempt {
+    val result = parse(text, statement(using _)).get.value
+    println(s"parsed: ${result.mkString("\n")}")
+    Nil
+  }
+
+  def statement[T: P]: P[Seq[Line]] = P(
+    (transaction | anyLine.map(Boring.apply)).rep
+  )
+
+  def transaction[T: P]: P[Transaction] =
+    P(
+      monthDay ~ " " ~
+        (!amount ~ AnyChar).rep.! ~
+        amount ~ " " ~
+        amount ~ eol.rep
+    ).map({ case (date, description, amount, balance) =>
+      Transaction(
+        date,
+        description = description.trim,
+        amount = amount,
+        balance = balance
+      )
+    })
+
+  sealed abstract class Line
+  case class Boring(s: String) extends Line
+  case class Transaction(
+      date: MonthDay,
+      description: String,
+      amount: BigDecimal,
+      balance: BigDecimal
+  ) extends Line
+
+  def amount[T: P] =
+    P(
+      ("-".? ~
+        digit.rep(1) ~
+        ("," ~ digit ~ digit ~ digit).rep ~
+        "." ~ digit ~ digit).!
+    ).map(amount => BigDecimal(amount.replace(",", "")))
+
+  val MonthDayFmt = DateTimeFormatter.ofPattern("MMM dd")
+  def monthDay[T: P] =
+    P((month ~ " " ~ day).!).map(s => MonthDay.parse(s, MonthDayFmt))
+
+  def month[T: P] = P(
+    IgnoreCase("Jan") |
+      IgnoreCase("Feb") |
+      IgnoreCase("Mar") |
+      IgnoreCase("Apr") |
+      IgnoreCase("May") |
+      IgnoreCase("Jun") |
+      IgnoreCase("Jul") |
+      IgnoreCase("Aug") |
+      IgnoreCase("Sep") |
+      IgnoreCase("Oct") |
+      IgnoreCase("Nov") |
+      IgnoreCase("Dec")
+  )
+  def day[T: P] = P(digit.rep(min = 1, max = 2))
+
+  def digit[T: P] = P(CharIn("0-9"))
+
+  def anyLine[T: P] = P((!eol ~ AnyChar).rep.! ~ eol)
+
+  def eol[T: P] = P("\n" | "\r\n" | "\r" | "\f")
+}

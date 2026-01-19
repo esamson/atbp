41acf8a4bc941141e9209a31f9c8cbbe5de35456
Edward Samson <edward@samson.ph>
2026-01-19 21:35:03 +0800

    Capture beginning balance

--- stmt2csv/src/main/scala/ph/samson/atbp/stmt2csv/parsers/BpiAccountParser.scala
+++ stmt2csv/src/main/scala/ph/samson/atbp/stmt2csv/parsers/BpiAccountParser.scala
@@ -12,15 +12,39 @@ import NoWhitespace.*
 
 object BpiAccountParser {
 
+  case class Statement(
+      beginningBalance: BigDecimal,
+      transactions: List[Transaction]
+  )
+
   def apply(text: String): Task[List[CsvEntry]] = ZIO.attempt {
-    val result = parse(text, statement(using _)).get.value
-    println(s"parsed: ${result.mkString("\n")}")
+    val stmt = parse(text, statement(using _)).get.value
+    convert(stmt)
+  }
+
+  def convert(stmt: Statement): List[CsvEntry] = {
+    println(
+      s"convert: ${stmt.beginningBalance}\n${stmt.transactions.mkString("\n")}"
+    )
+
     Nil
   }
 
-  def statement[T: P]: P[Seq[Line]] = P(
-    (transaction | anyLine.map(Boring.apply)).rep
-  )
+  def statement[T: P]: P[Statement] = P(
+    (!beginningBalance ~ anyLine).rep ~
+      beginningBalance ~
+      (transaction | anyLine.map(Boring.apply)).rep
+  ).map { case (_, balance, lines) =>
+    Statement(
+      balance.amount,
+      lines.toList.collect { case t: Transaction =>
+        t
+      }
+    )
+  }
+
+  def beginningBalance[T: P] =
+    P("BEGINNING BALANCE " ~ amount ~ eol.rep).map(BeginningBalance.apply)
 
   def transaction[T: P]: P[Transaction] =
     P(
@@ -39,6 +63,7 @@ object BpiAccountParser {
 
   sealed abstract class Line
   case class Boring(s: String) extends Line
+  case class BeginningBalance(amount: BigDecimal) extends Line
   case class Transaction(
       date: MonthDay,
       description: String,

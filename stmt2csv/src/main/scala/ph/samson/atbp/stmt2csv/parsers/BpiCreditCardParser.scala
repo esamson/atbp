4ad25761bf2eba14ed7dcbed40c1021a4a99638f
Edward Samson <edward@samson.ph>
2026-01-21 21:31:55 +0800

    Factor out extractText

--- stmt2csv/src/main/scala/ph/samson/atbp/stmt2csv/parsers/BpiCreditCardParser.scala
+++ stmt2csv/src/main/scala/ph/samson/atbp/stmt2csv/parsers/BpiCreditCardParser.scala
@@ -1,12 +1,26 @@
 package ph.samson.atbp.stmt2csv.parsers
 
-import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.text.PDFTextStripper
 import ph.samson.atbp.stmt2csv.CsvEntry
 import zio.Task
+import zio.ZIO
 
 object BpiCreditCardParser extends StatementParser {
 
-  override def extractText(doc: PDDocument): Task[String] = ???
+  override def stripper: PDFTextStripper = {
+    val s = new PDFTextStripper
+    s.setAverageCharTolerance(0.9f)
+    s.setSpacingTolerance(1.5f)
+    s
+  }
+
+  override def validate(text: String): Task[String] = if (
+    text.contains("www.bpi.com.ph")
+  ) {
+    ZIO.succeed(text)
+  } else {
+    ZIO.fail(new IllegalArgumentException("Not a BPI Statement"))
+  }
 
   override def parseEntries(text: String): Task[List[CsvEntry]] = ???
 }
